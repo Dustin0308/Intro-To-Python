1c0efb846a736175aef8da91c695ea515d5ceb2e
Dustin0308 <dustinreaver@gmail.com>
2025-05-09 22:32:23 +0000

    Data Types Exercises

---
+++ data_types/exercise_2.py
@@ -0,0 +1,17 @@
+# Create a tuple called names that contains several pet names. For isntance:
+
+# Asta
+# Butterscotch
+# Pudding
+# Neptune
+# Darwin
+
+names = (
+        'Asta',
+        'Butterscotch',
+        'Pudding',
+        'Neptune',
+        'Darwin',
+)
+
+print(names)
---
+++ data_types/exercise_3.py
@@ -0,0 +1,17 @@
+# Create a dictionary named pets that contains a list of pet names and type of animal. For insance:
+
+# Asta              dog
+# Butterscotch      cat
+# Pudding           cat
+# Neptune           fish
+# Darwin            lizard
+
+pets = {
+    'Asta': 'dog',
+    'Butterscotch': 'cat',
+    'Pudding': 'cat',
+    'Neptune': 'fish',
+    'Darwin': 'lizard',
+}
+
+print(pets)
